df943ed19d1e46b70ced3c35fe025a1049062535
Jeremy Shields <jeremytshields@outlook.com>
2025-07-27 13:40:31 -0500

    Personal Webpage
    Lesson 10 Webpage first draft

---
+++ L10HandsOn/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+  $('.icon').hover(
+    function () {
+      $(this).css('transform', 'scale(1.2)');
+    },
+    function () {
+      $(this).css('transform', 'scale(1)');
+    }
+  );
+
+  $.ajax({
+    url: 'https://api.github.com/users/JeremyTShields',
+    method: 'GET',
+    success: function (repos) {
+      repos.forEach(function (repo) {
+        $('#repo-list').append(`<li><a href="${repo.html_url}" target="_blank">${repo.name}</a></li>`);
+      });
+    }
+  });
+});
